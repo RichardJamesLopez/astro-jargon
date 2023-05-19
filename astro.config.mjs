55aa7750b74711d85f0fd81e208068d4718b028d
Rich CL <richcl@richs-air.lan>
2023-05-19 12:22:07 -0400

    Commit message

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import relativeLinks from "astro-relative-links";
+
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
+  integrations: [relativeLinks()]
 });
